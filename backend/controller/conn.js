50712b2941b2da9ffb9d53e13262305be4ff684b
WillyRumapea <131111329+WillyRumapea@users.noreply.github.com>
2025-07-29 22:31:11 +0700

    Update conn.js 3

--- backend/controller/conn.js
+++ backend/controller/conn.js
@@ -1,4 +1,9 @@
 const mysql = require("mysql2");
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "db_test_tengku",
+});
 
 module.exports = db;
